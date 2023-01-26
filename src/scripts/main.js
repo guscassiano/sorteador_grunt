aac242fff968aeb25b806469a34a443e4bbf6504
Gustavo Cassiano <gucpinto26@gmail.com>
2023-01-26 15:56:03 -0300

    First commit

---
+++ src/scripts/main.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function(){
+    document.getElementById('form-sorteador').addEventListener('submit', function(e){
+        e.preventDefault()
+        let nummax = parseInt(document.getElementById('num-max').value);
+
+        let numrandom = Math.random() * nummax;
+        numrandom = Math.round(numrandom)
+
+        if(numrandom == 0)
+            numrandom++
+
+        document.getElementById('resultado-valor').innerText = numrandom
+        document.querySelector('.resultado').style.display = 'block'
+    })
+})
